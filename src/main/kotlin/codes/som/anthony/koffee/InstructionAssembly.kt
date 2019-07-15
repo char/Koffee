61760e68add0765044d2fb6929fed80bfafa6719
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-07-16 00:33:18 +0100

    Move scope and mergeFrom from AbstractASMImpl to ASM

--- src/main/kotlin/codes/som/anthony/koffee/InstructionAssembly.kt
+++ src/main/kotlin/codes/som/anthony/koffee/InstructionAssembly.kt
@@ -12,15 +12,18 @@ interface TryCatchContainer { val tryCatchBlocks: MutableList<TryCatchBlockNode>
 
 interface ASM : LabelledASM, TryCatchContainer
 
-abstract class AbstractASMImpl(override val instructions: InsnList, override val tryCatchBlocks: MutableList<TryCatchBlockNode>) : ASM {
-    fun scope(routine: LabelScope.() -> Unit) {
-        routine(object : LabelScope {
-            override val L = this@AbstractASMImpl.L.scope(instructions)
-        })
-    }
+fun ASM.scope(routine: LabelScope.() -> Unit) {
+    val self = this
 
-    fun mergeFrom(asm: ASM, label: LabelWrapper) {
-        instructions.insert(label.labelNode, asm.instructions)
-        tryCatchBlocks.addAll(asm.tryCatchBlocks)
-    }
+    routine(object : LabelScope {
+        override val L = self.L.scope(instructions)
+    })
 }
+
+fun ASM.mergeFrom(asm: ASM, label: LabelWrapper) {
+    instructions.insert(label.labelNode, asm.instructions)
+    tryCatchBlocks.addAll(asm.tryCatchBlocks)
+}
+
+abstract class AbstractASMImpl(override val instructions: InsnList,
+        override val tryCatchBlocks: MutableList<TryCatchBlockNode>) : ASM
