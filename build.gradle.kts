b5083bd4dee67c5cf24ab944541cc9928dd6dd1c
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-12-19 00:57:35 +0000

    Breaking changes: Reorder name/type in 'fields', add 'interfaces' to assembleClass

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "codes.som.anthony"
-version = "6.4.0"
+version = "7.0.0"
 
 repositories {
     mavenCentral()
@@ -16,7 +16,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     arrayOf("asm", "asm-tree", "asm-commons").forEach {
-        implementation(group = "org.ow2.asm", name = it, version = "7.1")
+        implementation(group = "org.ow2.asm", name = it, version = "7.2")
     }
 }
 
--- src/main/kotlin/codes/som/anthony/koffee/ClassAssembly.kt
+++ src/main/kotlin/codes/som/anthony/koffee/ClassAssembly.kt
@@ -4,19 +4,20 @@ import codes.som.anthony.koffee.modifiers.Modifiers
 import codes.som.anthony.koffee.sugar.ModifiersAccess
 import codes.som.anthony.koffee.sugar.TypesAccess
 import codes.som.anthony.koffee.types.TypeLike
-import codes.som.anthony.koffee.types.coerceType
 import org.objectweb.asm.Opcodes.ASM7
-import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.Type
+import org.objectweb.asm.tree.ClassNode
 import org.objectweb.asm.tree.FieldNode
 import org.objectweb.asm.tree.MethodNode
+import codes.som.anthony.koffee.types.coerceType as coerceTypeStatic
 
 class ClassAssembly internal constructor(val node: ClassNode): ModifiersAccess, TypesAccess {
-    internal constructor(access: Modifiers, name: String, version: Int, superName: String) : this(ClassNode(ASM7).also {
+    internal constructor(access: Modifiers, name: String, version: Int, superName: String, interfaces: List<TypeLike>) : this(ClassNode(ASM7).also {
+        it.access = access.access
         it.name = name
         it.version = version
         it.superName = superName
-        it.access = access.access
+        it.interfaces = interfaces.map { type -> coerceTypeStatic(type).internalName }
     })
     
     var access: Modifiers
@@ -42,7 +43,7 @@ class ClassAssembly internal constructor(val node: ClassNode): ModifiersAccess, 
     val self: Type
         get() = coerceType(node.name)
 
-    fun field(access: Modifiers, type: TypeLike, name: String, signature: String? = null, value: Any? = null): FieldNode {
+    fun field(access: Modifiers, name: String, type: TypeLike, signature: String? = null, value: Any? = null): FieldNode {
         val fieldNode = FieldNode(ASM7, access.access, name, coerceType(type).descriptor, signature, value)
         node.fields.add(fieldNode)
         return fieldNode
@@ -63,8 +64,8 @@ class ClassAssembly internal constructor(val node: ClassNode): ModifiersAccess, 
     }
 }
 
-fun assembleClass(access: Modifiers, name: String, version: Int = 49, superName: String = "java/lang/Object", routine: ClassAssembly.() -> Unit): ClassNode {
-    val assembly = ClassAssembly(access, name, version, superName)
+fun assembleClass(access: Modifiers, name: String, version: Int = 49, superName: String = "java/lang/Object", interfaces: List<TypeLike> = listOf(), routine: ClassAssembly.() -> Unit): ClassNode {
+    val assembly = ClassAssembly(access, name, version, superName, interfaces)
     routine(assembly)
     return assembly.node
 }
