92e94d74afa9868afd0b99c3eff01b95dac51c4a
Jordin <me@jord.in>
2020-10-01 18:42:50 -0300

    Add return type parameter to call_super sugar

--- src/main/kotlin/codes/som/anthony/koffee/sugar/MethodAssemblyExtension.kt
+++ src/main/kotlin/codes/som/anthony/koffee/sugar/MethodAssemblyExtension.kt
@@ -6,8 +6,8 @@ import codes.som.anthony.koffee.insns.jvm.invokespecial
 import codes.som.anthony.koffee.types.TypeLike
 
 object MethodAssemblyExtension {
-    fun MethodAssembly.call_super(superClass: TypeLike = Object::class, name: String = "<init>") {
+    fun MethodAssembly.call_super(superClass: TypeLike = Object::class, name: String = "<init>", returnType: TypeLike = void) {
         aload_0 // load this
-        invokespecial(superClass, name, void)
+        invokespecial(superClass, name, returnType)
     }
 }
