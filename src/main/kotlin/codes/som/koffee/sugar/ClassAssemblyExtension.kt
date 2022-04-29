0c5fcbc15fa75be72cb3639172dbdbb9ee1c222f
sschr15 <steven.sschr15@hotmail.com>
2022-04-28 22:28:56 -0500

    Put super-call sugar on standby TODO maybe use Kotlin's experimental context modifier to do this?

--- src/main/kotlin/codes/som/koffee/sugar/ClassAssemblyExtension.kt
+++ src/main/kotlin/codes/som/koffee/sugar/ClassAssemblyExtension.kt
@@ -2,8 +2,9 @@ package codes.som.koffee.sugar
 
 import codes.som.koffee.ClassAssembly
 import codes.som.koffee.MethodAssembly
+import codes.som.koffee.insns.jvm.aload_0
+import codes.som.koffee.insns.jvm.invokespecial
 import codes.som.koffee.modifiers.Modifiers
-import codes.som.koffee.sugar.MethodAssemblyExtension.call_super
 import codes.som.koffee.types.TypeLike
 import org.objectweb.asm.tree.MethodNode
 import java.lang.IllegalArgumentException
@@ -13,7 +14,9 @@ public object ClassAssemblyExtension {
      * Creates a new constructor method.
      */
     public fun ClassAssembly.init(access: Modifiers, vararg parameterTypes: TypeLike,
-                                  superClass: TypeLike = Object::class, routine: MethodAssembly.() -> Unit): MethodNode {
+                                  superClass: TypeLike = Object::class, superParams: Array<TypeLike> = emptyArray(),
+                                  routine: MethodAssembly.() -> Unit
+    ): MethodNode {
         if (access.containsOther(public + private + protected + package_private)) {
             throw IllegalArgumentException("Method 'init' has illegal modifiers! " +
                     "Legal modifiers include: public, private, protected, and package private."
@@ -21,7 +24,8 @@ public object ClassAssemblyExtension {
         }
 
         return method(access, "<init>", void, *parameterTypes) {
-            call_super(superClass)
+            aload_0
+            invokespecial(superClass, "<init>", void, *superParams)
             routine()
         }
     }
