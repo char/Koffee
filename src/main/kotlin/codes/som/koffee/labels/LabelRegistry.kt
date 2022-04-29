74d34bd536eeba550ed8ff3f9c375d5d1da9d776
sschr15 <steven.sschr15@hotmail.com>
2022-04-29 07:37:17 -0500

    Add a bit more docs

--- src/main/kotlin/codes/som/koffee/labels/LabelRegistry.kt
+++ src/main/kotlin/codes/som/koffee/labels/LabelRegistry.kt
@@ -3,12 +3,22 @@ package codes.som.koffee.labels
 import codes.som.koffee.insns.InstructionAssembly
 import org.objectweb.asm.tree.LabelNode
 
+/**
+ * A wrapper around a [label] that can be easily added to an [InstructionAssembly].
+ */
 public class KoffeeLabel(private val insns: InstructionAssembly, internal val label: LabelNode) {
+    /**
+     * Add this label to the [InstructionAssembly].
+     */
     public operator fun unaryPlus() {
         insns.instructions.add(label)
     }
 }
 
+/**
+ * A storage for labels with a given [InstructionAssembly], so you don't
+ * have to keep track of them yourself.
+ */
 public class LabelRegistry(private val insns: InstructionAssembly) {
     private var labels = mutableMapOf<String, LabelNode>()
 
@@ -22,7 +32,14 @@ public class LabelRegistry(private val insns: InstructionAssembly) {
                 .also { it.labels.putAll(this.labels) }
     }
 
+    /**
+     * Get a label by an [index].
+     */
     public operator fun get(index: Int): KoffeeLabel = this["label_$index"]
+
+    /**
+     * Get a label by [name].
+     */
     public operator fun get(name: String): KoffeeLabel {
         return KoffeeLabel(insns, labels.getOrPut(name, ::LabelNode))
     }
