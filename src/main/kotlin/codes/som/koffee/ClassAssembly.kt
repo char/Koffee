05ed2f6e91c0c9376938767092b839236542073c
sschr15 <steven.sschr15@hotmail.com>
2022-04-29 07:37:17 -0500

    Add a bit more docs

--- src/main/kotlin/codes/som/koffee/ClassAssembly.kt
+++ src/main/kotlin/codes/som/koffee/ClassAssembly.kt
@@ -23,42 +23,77 @@ public class ClassAssembly internal constructor(public val node: ClassNode): Mod
         it.superName = superName
         it.interfaces = interfaces.map { type -> coerceTypeStatic(type).internalName }
     })
-    
+
+    /**
+     * The access flags (i.e. [public], [abstract], [enum], etc.) of this class.
+     */
     public var access: Modifiers
         get() = Modifiers(node.access)
         set(value) { node.access = value.access }
-    
+
+    /**
+     * The slash-separated fully qualified name of this class.
+     *
+     * Example: The default Java `Object` class is `java/lang/Object`.
+     */
     public var name: String
         get() = node.name
         set(value) { node.name = value }
-    
+
+    /**
+     * The class version number. A chart can be found in the
+     * [JVM specification](https://docs.oracle.com/javase/specs/jvms/se18/html/jvms-4.html#jvms-4.1).
+     */
     public var version: Int
         get() = node.version
         set(value) { node.version = value }
-    
+
+    /**
+     * The superclass of this class, as an ASM [Type].
+     * It should only be `null` for Java's `Object` class.
+     */
     public var superClass: Type?
         get() = node.superName?.let(::coerceType)
         set(value) { node.superName = value?.internalName }
 
-    public var interfaces: List<Type>?
-        get() = node.interfaces?.map(::coerceType)
-        set(value) { node.interfaces = value?.map { it.internalName } }
+    /**
+     * The interfaces implemented by this class, as ASM [Type]s.
+     */
+    public var interfaces: List<Type>
+        get() = node.interfaces.map(::coerceType)
+        set(value) { node.interfaces = value.map { it.internalName } }
 
+    /**
+     * This class as an ASM [Type].
+     */
     public val self: Type
         get() = coerceType(node.name)
 
+    /**
+     * Create a new field with the given information.
+     *
+     * If it is a static final field using a constant value, [value] can be used
+     * instead of initialization during `<clinit>`.
+     */
     public fun field(access: Modifiers, name: String, type: TypeLike, signature: String? = null, value: Any? = null): FieldNode {
         val fieldNode = FieldNode(ASM7, access.access, name, coerceType(type).descriptor, signature, value)
         node.fields.add(fieldNode)
         return fieldNode
     }
 
+    /**
+     * Create a new method with the given information.
+     *
+     * Although exceptions can be specified to be thrown using [exceptions], the JVM
+     * does no exception checking at runtime. Therefore, any undeclared exceptions get
+     * thrown without the caller requiring handling.
+     */
     public fun method(access: Modifiers, name: String, returnType: TypeLike, vararg parameterTypes: TypeLike,
-                      signature: String? = null, exceptions: Array<Type>? = null,
+                      signature: String? = null, exceptions: Array<TypeLike>? = null,
                       routine: MethodAssembly.() -> Unit): MethodNode {
         val descriptor = Type.getMethodDescriptor(coerceType(returnType), *parameterTypes.map(::coerceType).toTypedArray())
 
-        val methodNode = MethodNode(ASM7, access.access, name, descriptor, signature, exceptions?.map { it.internalName }?.toTypedArray())
+        val methodNode = MethodNode(ASM7, access.access, name, descriptor, signature, exceptions?.map { coerceType(it).internalName }?.toTypedArray())
         val methodAssembly = MethodAssembly(methodNode)
         routine(methodAssembly)
 
@@ -69,7 +104,7 @@ public class ClassAssembly internal constructor(public val node: ClassNode): Mod
 }
 
 /**
- * Assemble a class and return the resulting [ClassNode].
+ * Assemble a class with the given information and creation block, and return the resulting [ClassNode].
  */
 public fun assembleClass(access: Modifiers, name: String, version: Int = V1_5, superName: String = "java/lang/Object", interfaces: List<TypeLike> = listOf(), routine: ClassAssembly.() -> Unit): ClassNode {
     val assembly = ClassAssembly(access, name, version, superName, interfaces)
--- src/main/kotlin/codes/som/koffee/labels/LabelScope.kt
+++ src/main/kotlin/codes/som/koffee/labels/LabelScope.kt
@@ -1,5 +1,14 @@
 package codes.som.koffee.labels
 
 public interface LabelScope {
+    /**
+     * This is a short name used to easily create and reference labels:
+     * ```kt
+     * // ...
+     * +L["foo"]
+     * // ...
+     * goto(L["foo"])
+     * ```
+     */
     public val L: LabelRegistry
 }
